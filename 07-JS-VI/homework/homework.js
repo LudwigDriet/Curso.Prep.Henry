ea0eebf9d18448fba102c55ed8bbb395bf41706c
LudwigDriet <ludwigmisael95@gmail.com>
2021-08-12 11:07:06 -0300

    tarea VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -39,7 +39,7 @@ function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
-  var newArray = array.forEach(function(ele){
+  array.forEach(function(ele){
     cb(ele)
   })
 }
